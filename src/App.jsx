219ff2f3c67238ac7fab71f491061fd17b0c7f3b
Sam Sh <saby24@student.bth.se>
2025-09-12 01:10:08 +0200

    Resolve conflicts due to moving files to separate directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Docs from "./pages/Docs.jsx";
-import Header from "./components/Header.jsx";
+import Docs from "./pages/Docs/Docs.jsx";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import "./App.css";
 
 function App() {
