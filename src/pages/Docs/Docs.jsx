c4a0cb3a6873d27b2fda179539c47557a1a22b5e
Hampus <Hampe024@gmail.com>
2025-09-11 18:59:43 +0200

    mv components to seperate folders

---
+++ src/pages/Docs/Docs.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { getAllData } from "../../api/data";
+import "./Docs.css";
+
+function Docs() {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getAllData()
+      .then((result) => setData(result.docs))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => navigate("/add")}>Add new</button>
+      <h2>Documents</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.content}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Docs;
